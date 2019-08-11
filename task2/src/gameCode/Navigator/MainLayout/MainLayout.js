1f78b89544631555647b6a6afda71678ac190c1c
Neha Gupta <nehagupta0595@gmail.com>
2019-08-11 14:04:02 +0530

    Remove unused code

--- task2/src/gameCode/Navigator/MainLayout/MainLayout.js
+++ task2/src/gameCode/Navigator/MainLayout/MainLayout.js
@@ -26,32 +26,3 @@ class MainLayout extends React.Component {
 }
 
 export default MainLayout;
-
-
-// import React from 'react'
-// import {
-//   BrowserRouter,
-//   Redirect,
-//   Route
-// } from 'react-router-dom'
-
-// import {
-//   Cards,
-//   EndGame,
-//   MainMenu,
-//   NewGame
-// } from '../../components'
-
-// const MainLayout = ({ className }) => (
-//   <BrowserRouter>
-//     <div className={className}>
-//       <Route exact path="/" render={() => <Redirect to="/menu" />} />
-//       <Route path="/new-game" component={NewGame} />
-//       <Route path="/game" component={Cards} />
-//       <Route path="/menu" component={MainMenu} />
-//       <Route path="/end-game" component={EndGame} />
-//     </div>
-//   </BrowserRouter>
-// )
-
-// export default MainLayout
